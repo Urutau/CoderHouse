6ab6832d6e3384c445c9aab27079131fcee47329
Cristina Cecilia Ortega <xristinaortega@gmail.com>
2021-12-27 10:29:12 -0300

    Versión con listado impreso de productos financieros.

--- events/events.js
+++ events/events.js
@@ -21,4 +21,30 @@ let arrow = document.getElementById("arrow");
 arrow.onmouseover = () => cambiarArrow();
 
 let arrow2 = document.getElementById("arrow");
- arrow2.onmouseleave = () => cambiarArrow2();
+arrow2.onmouseleave = () => cambiarArrow2();
+
+let bListado = document.getElementById("bListado");
+bListado.onclick = () => mostrarProductos();
+
+function mostrarProductos () {
+    let divProductos = document.getElementById("divProductos");
+    let tituloListado = document.createElement("h2");
+    tituloListado.innerHTML = `<h2>Listado actual de productos financieros:</h2>`
+    divProductos.appendChild(tituloListado);
+    let listado = document.createElement("p");
+    productos.forEach(producto => {
+        listado.innerHTML += `<p>
+        Producto: ${producto.nombre}
+        <br>
+        Duración: ${producto.duracion} días
+        <br>
+        Volatilidad: ${producto.volatilidad}
+        <br>
+        Liquidez: en ${producto.liquidez} hs
+        <br>
+        Beneficio: coeficiente ${producto.beneficio}
+        </p>`
+        divProductos.appendChild(listado)
+    })
+        
+}
