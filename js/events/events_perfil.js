e7bd533462a77fe1eee6a628f0eaff0f58610d3d
Cristina Cecilia Ortega <xristinaortega@gmail.com>
2022-01-14 20:13:51 -0300

    Cambios de estilo.

--- js/events/events_perfil.js
+++ js/events/events_perfil.js
@@ -24,9 +24,9 @@ function submit (e) {
     perfilDeInversor(acumulativo);
     $("#cuestionario").append(`<p class="perfil" id="perfil">Tu perfil de inversión es:\n${perfil}</p>`);
     $("#formulario").slideUp(2000, ()=>{
-        $("#cuestionario").append(`<button id="bRecomendacion">Recomendaciones según tu perfil de inversor</button>`);
+        $("#cuestionario").append(`<button class="buttonEnviar" id="bRecomendacion">Recomendaciones según tu perfil de inversor</button>`);
         $("#bRecomendacion").animate({
             width: "100%",
-        }, 2000)
+        }, 2000);
     })
 }
